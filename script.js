5dc45b686075fdcae56739c63652ad67660b80d9
AJ <abdijabarboxer@gmail.com>
2020-07-18 01:06:42 +0300

    added hide alert option to script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+/*hide the alert display*/
+/* this will hide the alert display before we submit the dates to the form */
+
+document.getElementById("alert1").style.display = "none";
+document.getElementById("alert2").style.display = "none";
+
