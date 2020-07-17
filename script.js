0e26643d85c651d4ec9353425bd3259b742654a3
AJ <abdijabarboxer@gmail.com>
2020-07-18 01:12:38 +0300

    added female names array, male names array & days of the week array

--- script.js
+++ script.js
@@ -4,3 +4,9 @@
 document.getElementById("alert1").style.display = "none";
 document.getElementById("alert2").style.display = "none";
 
+/*declare an array for Akan names*/
+/* Const used because the names and days of the weeks will be the same*/
+
+const femaleAkanNames = [	"Akosua,=",  "Adwoa,",	"Abenaa,", 	"Akua,",	"Yaa,", "Afua,",  "Ama," ];
+const maleAkanNames   = [	"Kwasi,",   "Kwadwo,",  "Kwabena,",	  "Kwaku,",	  "Yaw,",	 "Kofi,",	"Kwame,",];
+const daysWeek        = [  "Sunday",  "Monday",	"Tuesday",	"Wednesday",	"Thursday",	  "Friday", "Saturday" ];
