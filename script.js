95a9c83e57d27f34b828faec5b86b1a122c9b011
AJ <abdijabarboxer@gmail.com>
2020-07-18 01:24:19 +0300

    added date and month value controller to script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 /*hide the alert display*/
-/* this will hide the alert display before we submit the dates to the form */
+/* this will hide the alert display before we submit the date to the form */
 
 document.getElementById("alert1").style.display = "none";
 document.getElementById("alert2").style.display = "none";
+
+
 
 /*declare an array for Akan names*/
 /* Const used because the names and days of the weeks will be the same*/
@@ -10,3 +12,25 @@ document.getElementById("alert2").style.display = "none";
 const femaleAkanNames = [	"Akosua,=",  "Adwoa,",	"Abenaa,", 	"Akua,",	"Yaa,", "Afua,",  "Ama," ];
 const maleAkanNames   = [	"Kwasi,",   "Kwadwo,",  "Kwabena,",	  "Kwaku,",	  "Yaw,",	 "Kofi,",	"Kwame,",];
 const daysWeek        = [  "Sunday",  "Monday",	"Tuesday",	"Wednesday",	"Thursday",	  "Friday", "Saturday" ];
+
+
+
+/*correct day controller*/
+/*sets a limit for the 1-31 days in a month*/
+
+if (day <= 0 || day >= 32) {
+    alert(
+        "please input a valid date"
+    );
+}
+
+
+/*correct month controller*/
+/*sets a limit for the 1-12 Months in a year*/
+
+if (month <= 0 || month >= 13) {
+    alert(
+        "Please input a valid month"
+    );
+}
+
